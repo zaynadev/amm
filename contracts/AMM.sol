499e666a2926c79a036c70b07e61ee8da428e706
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-13 23:53:55 +0100

    addLiquidity function

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -45,5 +45,48 @@ contract AMM {
         // update the reserve
         _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
     }
+
+    function addLiquidity(uint _amount0, uint _amount1) external returns(uint shares){
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+        if(reserve0 > 0 || reserve1 > 0){
+            require(reserve0 * _amount1 == reserve1 * _amount0, "dy/dx != y/x");
+        }
+        // value of liquidity = sqrt(xy)
+        // s = dx / x * T = dy / y * T
+        // T = total shares
+        // s = shares to mint
+
+        if (totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
+        } else {
+            shares = _min(
+                (_amount0 * totalSupply) / reserve0,
+                (_amount1 * totalSupply) / reserve1
+            );
+        }
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+
+        _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
+
+    }
+
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
+    }
+
+    function _sqrt(uint y) private pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
   
 }
